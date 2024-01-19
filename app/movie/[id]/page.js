9072f56eaf2b6e428f47e5faf795a195e3f183ba
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2024-01-19 16:27:49 +0300

    Detay sayfası yapılacak

---
+++ app/movie/[id]/page.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import MovieContainer from '@/containers/movie'
+import Movies from '@/mocks/movies.json'
+import { notFound } from 'next/navigation'
+
+ async function delay(ms){
+  return new Promise((resolve)=>setTimeout(resolve, ms));
+}
+async function MoviePage({params,searchParams}) {
+   await delay(2000);
+    const movieDetails = Movies.results.find((movie)=>movie.id.toString()=== params.id)
+    if (!movieDetails) {
+        notFound();/** Eğer id bulunamzsa buraya yönlendirecek */
+    }
+    if (searchParams.error ==="true") {
+        throw new Error("Error happened");
+    }
+  return (
+    <MovieContainer movie={movieDetails}/>
+  )
+}
+
+export default MoviePage
--- containers/home/index.js
+++ containers/home/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Movies from '@/mocks/movies.json'
+import Genres from '@/mocks/genres.json'
 import FeaturedMovie  from '@/components/featured-movie'
+import Categories from '@/components/categories'
+import MovieSection from '@/components/movie-sections'
 function HomeContainer() {
   return (
     <div>
       <FeaturedMovie  movie={Movies.results[0]}/>
+      <Categories categories={Genres.genres.slice(0,5)}/>
+      <MovieSection title="Popular Films" movies={Movies.results.slice(1,7)}/>
+      <MovieSection title="Your Favorites" movies={Movies.results.slice(7,13)}/>
       </div>
   )
 }
