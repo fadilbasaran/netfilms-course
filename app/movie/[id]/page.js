650437c173be672c3fd942426982d960964bca91
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2024-02-07 10:44:36 +0300

    movie kısmının notFound sayfası düzenlendi.

--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -4,31 +4,18 @@ import MovieContainer from '@/containers/movie'
 import { notFound } from 'next/navigation'
 import { getMovie } from '@/services/movie';
 
-// async function delay(ms) {
-//   return new Promise((resolve) => setTimeout(resolve, ms));
-// }
-// const API_URL = "https://api.themoviedb.org/3"
-
-// const getMovie= async (movieId)=>{
-//     const rest = await fetch(`${API_URL}/movie/${movieId}?api_key=${process.env.API_KEY}`);
-//     return rest.json(); 
-// } 
-
 
 async function MoviePage({ params, searchParams }) {
-  //  await delay(2000);
-  const movieDetails = await getMovie(params.id);
-  // const movieDetails = Movies.results.find((movie) => movie.id.toString() === params.id)
-  
-  if (!movieDetails) {
-    notFound();/** Eğer id bulunamzsa buraya yönlendirecek */
+  const movieDetail = await getMovie(params.id);
+  if (movieDetail.success === false) {
+    notFound();
   }
-  // if (searchParams.error === "true") {
-  //   throw new Error("Error happened");
-  // }
-  return (
-    <MovieContainer movie={movieDetails} />
-  )
+
+  if (searchParams.error === "true") {
+    throw new Error("Something went wrong!");
+  }
+  return <MovieContainer movie={movieDetail} />;
+  
 }
 
 export default MoviePage
